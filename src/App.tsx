1db0933884711a3960cc3285fb187c7f3cb8a298
nihal-soni <nihalsoni0523@gmail.com>
2025-08-01 20:55:45 +0530

    feat: added tanstack-query and api key

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import Layout from "./components/layout"
-import { BrowserRouter, Route , Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { ThemeProvider } from "./context/theme-provider"
 import WeatherDashboard from "./pages/weather-dashboard"
 import CityPage from "./pages/city-page"
-
+import { QueryClientProvider , QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard/>} />
-            <Route path="/City/:cityname" element={<CityPage/>} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/City/:cityname" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
